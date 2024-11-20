c3e9600b80ea7a12bb718723fd9d1a2de95ff89f
reinami <typhonnge@gmail.com>
2024-11-20 00:29:39 -0800

    yes

--- neovim/.config/nvim/lua/reinami/lazy_init.lua
+++ neovim/.config/nvim/lua/reinami/lazy_init.lua
@@ -1,4 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy_packages/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
@@ -13,6 +13,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    spec = "reinami.lazy_packages",
+    spec = "reinami.lazy",
     change_detection = { notify = false }
 })
